7151739cef8250ef3212d3dc0874c04b8b9ff4ab
Reece Dunham <me@rdil.rocks>
2022-03-06 14:15:48 -0500

    Fix minor crash

--- src/main/groovy/org/thepeacockproject/idea/editor/MenuFileEditor.java
+++ src/main/groovy/org/thepeacockproject/idea/editor/MenuFileEditor.java
@@ -5,6 +5,7 @@ import com.intellij.openapi.fileEditor.FileEditorLocation;
 import com.intellij.openapi.fileEditor.FileEditorState;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.ui.components.JBLabel;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -32,6 +33,10 @@ public class MenuFileEditor implements FileEditor {
     @Override
     @NotNull
     public JComponent getComponent() {
+        if (this.editor == null) {
+            return new JBLabel("Something went wrong, try again later please!");
+        }
+
         return this.editor;
     }
 
